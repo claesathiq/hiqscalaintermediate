612a1fd77f598fbd23287940477f2408138d180e
Claes Jonsson <claes@bbschk.com>
2014-05-02 16:06:52 +0200

    Step 2

--- src/com/hiq/scala/intermediate/PartialFunction.scala
+++ src/com/hiq/scala/intermediate/PartialFunction.scala
@@ -1,19 +1,12 @@
 package com.hiq.scala.intermediate
 
 /**
- * Partial functions, introduction
+ * Partial functions, continued
  */
 object PartialFunction extends App {
-  val salaries = Map("junior" -> 35000, "normal" -> 45000, "senior" -> 55000, "boss" -> 70000)
-
-  // Map.filter takes a predicate function, in this case p: ((String, Int)) => Boolean
-  //   we get a tuple, so we have get key with position                           ↓
-  val highSalaryFunc = salaries.filter((nameSalary: (String, Int)) => nameSalary._2 > 50000)
-  println(highSalaryFunc)
-
-  // we can actually use pattern matching to extract key and value
-  val highSalaryMatch = salaries.filter({case (name, salary) => salary > 50000})
-  println(highSalaryMatch)
-
-  // So if Map.filter takes a function, how can a pattern match work?
+  val one: PartialFunction[Int, String] = { case 1 => "one" }
+  println(one.isDefinedAt(1))
+  println(one.isDefinedAt(2))
+  println(one(1))
+  // println(one(2))  // Throws a scala.MatchError
 }
