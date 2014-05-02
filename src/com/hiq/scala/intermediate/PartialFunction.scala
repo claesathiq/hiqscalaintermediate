865039fa3bda32d467ffabbb7fcd80de05948d64
Claes Jonsson <claes@bbschk.com>
2014-05-02 16:17:24 +0200

    Step 3

--- src/com/hiq/scala/intermediate/PartialFunction.scala
+++ src/com/hiq/scala/intermediate/PartialFunction.scala
@@ -1,12 +1,32 @@
 package com.hiq.scala.intermediate
 
 /**
- * Partial functions, continued
+ * Partial functions, composing
  */
 object PartialFunction extends App {
   val one: PartialFunction[Int, String] = { case 1 => "one" }
-  println(one.isDefinedAt(1))
-  println(one.isDefinedAt(2))
-  println(one(1))
-  // println(one(2))  // Throws a scala.MatchError
+  val two: PartialFunction[Int, String] = { case 2 => "two" }
+  val three: PartialFunction[Int, String] = { case 3 => "three" }
+  val wildcard: PartialFunction[Int, String] = { case _ => "something else" }
+
+  // Compose a partial function
+  val partial = one orElse two orElse three orElse wildcard
+
+  println(partial(1))
+  println(partial(3))
+  println(partial(0))
+  println(partial(5))
+
+  // Note the similarity to a pattern match
+  def patternMatch(x: Int): String = x match {
+    case 1 => "one"
+    case 2 => "two"
+    case 3 => "three"
+    case _ => "something else"
+  }
+
+  println(patternMatch(1))
+  println(patternMatch(3))
+  println(patternMatch(0))
+  println(patternMatch(5))
 }
