73d133b571103d07fd1c37846e20fcfcec6e5da8
Claes Jonsson <claes@bbschk.com>
2014-05-05 01:11:39 +0200

    Start course

---
+++ src/com/hiq/scala/intermediate/Intermdiate.scala
@@ -0,0 +1,12 @@
+package com.hiq.scala.intermediate
+
+/**
+ * Created: 2014-05-05 01:09
+ *
+ * @author Claes Jonsson
+ */
+object Intermdiate extends App {
+
+  println("""Start the course by issuing "git checkout -f step1"""")
+
+}
