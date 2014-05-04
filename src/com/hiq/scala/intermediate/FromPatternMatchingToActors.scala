7a6511614d8cf765683cbd299e6149108cebf6d0
Claes Jonsson <claes@bbschk.com>
2014-05-05 01:01:44 +0200

    Exc 1

--- src/com/hiq/scala/intermediate/FromPatternMatchingToActors.scala
+++ src/com/hiq/scala/intermediate/FromPatternMatchingToActors.scala
@@ -6,19 +6,28 @@ import scala.concurrent.duration._
 import com.typesafe.config.ConfigFactory
 
 /**
- * From Pattern Matching to Actors - the Actor demo
+ * From Pattern Matching to Actors - the Actor exercise
  */
 object FromPatternMatchingToActors extends App {
+  // nrOfWorkers = 4,       the number of workers to run in the pool, tweak according to available CPUs
+  // nrOfElements = 10000,  the number of elements in a chunk: 1/n, 1/(n+1) ... 1/(n + nrOfElements)
+  // nrOfMessages = 10000,  the number of chunks (or put another way, the number of messages sent to Worker)
   calculate(nrOfWorkers = 4, nrOfElements = 10000, nrOfMessages = 10000)
 
+  // Suggested messages
   sealed trait PiMessage
+  // Starts the show
   case object Calculate extends PiMessage
+  // Calculate a chunk
   case class Work(start: Int, nrOfElements: Int) extends PiMessage
+  // Return value from a Worker
   case class Result(value: Double) extends PiMessage
+  // The end result from Master
   case class PiApproximation(pi: Double, duration: Duration)
 
   class Worker extends Actor {
 
+    // The calculation of a chunk
     def calculatePiFor(start: Int, nrOfElements: Int): Double = {
       var acc = 0.0
       for (i <- start until (start + nrOfElements))
@@ -27,57 +36,42 @@ object FromPatternMatchingToActors extends App {
     }
 
     def receive = {
-      case Work(start, nrOfElements) =>
-        sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
-    }
+      // Receive Work, calculate, send Result back
   }
 
   class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef) extends Actor {
 
+    // result accumulator
     var pi: Double = _
+    // track number of returned results
     var nrOfResults: Int = _
+    // timing
     val start: Long = System.currentTimeMillis
 
     val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(nrOfWorkers)), name = "workerRouter")
 
     def receive = {
-      case Calculate =>
-        for (i <- 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
-      case Result(value) =>
-        pi += value
-        nrOfResults += 1
-        if (nrOfResults == nrOfMessages) {
-          // Send the result to the listener
-          listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
-          // Stops this actor and all its supervised children: the router and the router's children
-          context.stop(self)
-        }
+      // Receive Calculate,  immediately send appropriate chunks to all workers, via the router
+
+      // Receive Result from each Worker, accumulate in pi, check how many results we have
+      //    if we have all results, send PiApproximation to listener and stop self and all children
     }
   }
 
   class Listener extends Actor {
     def receive = {
-      case PiApproximation(pi, duration) =>
-        println("\n\tPi approximation: \t%s\n\tCalculation time: \t%s".format(pi, duration))
-        context.system.shutdown()
-    }
+      // Receive PiApproximation, print and shut down system
   }
 
   def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
     // Create an Akka system
-    // Sometimes the Terminate message from Worker to another Worker will not be received before termination and thus
-    // go into the dead letter box, which is logged as a warning; the following setting will prevent that
-    val system = ActorSystem("PiSystem", ConfigFactory.parseString("""
-      akka {
-         log-dead-letters-during-shutdown = off
-      }
-      """))
+    val system = ...
 
     // create the result listener, which will print the result and shutdown the system
-    val listener = system.actorOf(Props[Listener], name = "listener")
+    val listener = ...
 
     // create the master
-    val master = system.actorOf(Props(new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener)), name = "master")
+    val master = ...
 
     // start the calculation
     master ! Calculate
