fe801f67b995f070de120811f0fe503e6e4ff234
Claes Jonsson <claes@bbschk.com>
2014-05-05 00:06:38 +0200

    demo1

--- src/com/hiq/scala/intermediate/FromPatternMatchingToActors.scala
+++ src/com/hiq/scala/intermediate/FromPatternMatchingToActors.scala
@@ -1,33 +1,86 @@
 package com.hiq.scala.intermediate
 
-import akka.actor.{Actor, Props}
-import akka.routing.RoundRobinRouter
+import akka.actor._
+import akka.routing.RoundRobinPool
+import scala.concurrent.duration._
+import com.typesafe.config.ConfigFactory
 
 /**
- * From Pattern Matching to Actors - Actor routing the easy way
+ * From Pattern Matching to Actors - the Actor demo
  */
 object FromPatternMatchingToActors extends App {
+  calculate(nrOfWorkers = 4, nrOfElements = 10000, nrOfMessages = 10000)
 
-  case class Work()
-  case class Result(value: Int)
+  sealed trait PiMessage
+  case object Calculate extends PiMessage
+  case class Work(start: Int, nrOfElements: Int) extends PiMessage
+  case class Result(value: Double) extends PiMessage
+  case class PiApproximation(pi: Double, duration: Duration)
 
   class Worker extends Actor {
+
+    def calculatePiFor(start: Int, nrOfElements: Int): Double = {
+      var acc = 0.0
+      for (i <- start until (start + nrOfElements))
+        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+      acc
+    }
+
     def receive = {
-      case w: Work => println("Working...")
+      case Work(start, nrOfElements) =>
+        sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
     }
   }
 
-  class Master extends Actor {
+  class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef) extends Actor {
 
-    // To specify a supervisor strategy, do
-    // RoundRobinRouter(5).withSupervisorStrategy(yourStrategy)
-    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinRouter(5)), name = "workerRouter")
+    var pi: Double = _
+    var nrOfResults: Int = _
+    val start: Long = System.currentTimeMillis
+
+    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(nrOfWorkers)), name = "workerRouter")
 
     def receive = {
-      case w: Work =>
-        for (i <- 0 until 10000) workerRouter ! w
+      case Calculate =>
+        for (i <- 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
       case Result(value) =>
-        println("Result is " + value)
+        pi += value
+        nrOfResults += 1
+        if (nrOfResults == nrOfMessages) {
+          // Send the result to the listener
+          listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
+          // Stops this actor and all its supervised children: the router and the router's children
+          context.stop(self)
+        }
     }
+  }
+
+  class Listener extends Actor {
+    def receive = {
+      case PiApproximation(pi, duration) =>
+        println("\n\tPi approximation: \t%s\n\tCalculation time: \t%s".format(pi, duration))
+        context.system.shutdown()
+    }
+  }
+
+  def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
+    // Create an Akka system
+    // Sometimes the Terminate message from Worker to another Worker will not be received before termination and thus
+    // go into the dead letter box, which is logged as a warning; the following setting will prevent that
+    val system = ActorSystem("PiSystem", ConfigFactory.parseString("""
+      akka {
+         log-dead-letters-during-shutdown = off
+      }
+      """))
+
+    // create the result listener, which will print the result and shutdown the system
+    val listener = system.actorOf(Props[Listener], name = "listener")
+
+    // create the master
+    val master = system.actorOf(Props(new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener)), name = "master")
+
+    // start the calculation
+    master ! Calculate
+
   }
 }
