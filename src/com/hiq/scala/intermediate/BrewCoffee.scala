570cfc1b1493b6071d4d4a7097721eab194c7c33
Claes Jonsson <claes@bbschk.com>
2014-05-11 15:29:17 +0200

    Step 12

---
+++ src/com/hiq/scala/intermediate/BrewCoffee.scala
@@ -0,0 +1,31 @@
+package com.hiq.scala.intermediate
+
+import scala.util.Try
+
+/**
+ * Sequential coffee brewing
+ */
+object BrewCoffee extends App {
+  type CoffeeBeans = String
+  type GroundCoffee = String
+  case class Water(temperature: Int)
+  type Milk = String
+  type FrothedMilk = String
+  type Espresso = String
+  type Cappuccino = String
+
+  // dummy implementations of the individual steps:
+  def grind(beans: CoffeeBeans): GroundCoffee = s"ground coffee of $beans"
+  def heatWater(water: Water): Water = water.copy(temperature = 85)
+  def frothMilk(milk: Milk): FrothedMilk = s"frothed $milk"
+  def brew(coffee: GroundCoffee, heatedWater: Water): Espresso = "espresso"
+  def combine(espresso: Espresso, frothedMilk: FrothedMilk): Cappuccino = "cappuccino"
+
+  // going through these steps sequentially:
+  def prepareCappuccino(): Try[Cappuccino] = for {
+    ground <- Try(grind("arabica beans"))
+    water <- Try(heatWater(Water(25)))
+    espresso <- Try(brew(ground, water))
+    foam <- Try(frothMilk("milk"))
+  } yield combine(espresso, foam)
+}
